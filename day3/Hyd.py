65a3cfa57e651aef015d1fa2ac365ca3ae6daf35
Jahnavireddycnu <37660597+Jahnavireddycnu@users.noreply.github.com>
2020-01-08 03:31:01 -0800

    Add files via upload

---
+++ day3/Hyd.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def climate():
+    print("moderate to hot")
+
+
+# In[2]:
+
+
+def FoodHabits():
+    print("medium spicy")
+
+
+# In[3]:
+
+
+def TouristAttractions():
+    print("Golconda Fort")
+    print("Charminar")
+    print("Falaknama Palace")
+
+
+# In[4]:
+
+
+a=12
+b=21
+
+
+# In[ ]:
+
+
+
+
---
+++ day3/Kurnool.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def climate():
+    print("Very hot")
+
+
+# In[2]:
+
+
+def FoodHabits():
+    print("Loves very very spicy food")
+
+
+# In[4]:
+
+
+def TouristPlaces():
+    print("Konda Reddy Fort")
+    print("Srisailam")
+    print("Mantralayam")
+    print("Nallamalla Tiger Reserve")
+    print("Ahobilam")
+    print("Mahanandi")
+
+
+# In[5]:
+
+
+a = 60
+b = 40
+
+
+# In[ ]:
+
+
+
+
